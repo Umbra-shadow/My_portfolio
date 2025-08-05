7c8cd1bc180c1d75a699de714453319333f98a96
umbra igor <umbraigor@gmail.com>
2025-08-05 15:50:26 +0300

    Updated `serviceWorkerVersion` in `flutter_bootstrap.js`
    The `serviceWorkerVersion` in `flutter_bootstrap.js` has been updated. The hash for `flutter_bootstrap.js` in `flutter_service_worker.js` and the build ID in `.last_build_id` were also updated accordingly. Additionally, `.build/` was removed from `.gitignore`.

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -2142,7 +2142,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "dc8dff59835d9933b7a929899131faee
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "93523962ec3b090708f7e076fb013712",
+"flutter_bootstrap.js": "22a4a6fef739d852324275a149b5f113",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
